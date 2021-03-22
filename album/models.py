1fb557f5a43941721ac49f006d034f7eae00b7c3
codebr3ak <barakaralph@gmail.com>
2021-03-23 02:52:34 +0300

    Image data

--- album/models.py
+++ album/models.py
@@ -5,3 +5,12 @@ class Image(models.Model):
     name = models.CharField(max_length= 60)
     pic = models.ImageField(upload_to = 'uploads')
     description = models.TextField()
+
+     @classmethod
+    def save_image(cls):
+        image = cls.objects.all()
+        return image
+
+
+    def __str__(self):
+        return self.name 
